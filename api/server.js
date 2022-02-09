9683188c945bff71e988d037dad536e6d0487661
Allison Stewart <amstewart@yahoo.com>
2022-02-09 16:16:28 -0500

    set up logger middleware

--- api/server.js
+++ api/server.js
@@ -3,10 +3,10 @@ const express = require('express');
 const server = express();
 
 const usersRouter = require('./users/users-router.js');
-// const logger = require('./middleware/middleware');
+const { logger } = require('./middleware/middleware');
 server.use(express.json());
 server.use('/api/users', usersRouter);
-// server.use(logger);
+server.use(logger);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
