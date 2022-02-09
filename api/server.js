9107369737588b4a2c61c4f21fd5959740c976ec
Allison Stewart <amstewart@yahoo.com>
2022-02-09 16:03:47 -0500

    set up server and api to get all users

--- api/server.js
+++ api/server.js
@@ -2,12 +2,20 @@ const express = require('express');
 
 const server = express();
 
-// remember express by default cannot parse JSON in request bodies
-
-// global middlewares and the user's router need to be connected here
+const usersRouter = require('./users/users-router.js');
+// const logger = require('./middleware/middleware');
+server.use(express.json());
+server.use('/api/users', usersRouter);
+// server.use(logger);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
+
+// server.use('*', (req, res) => {
+//   res.status(404).json({
+//     message: `${req.meth} ${req.baseUrl} not found`
+//   });
+// });
 
 module.exports = server;
